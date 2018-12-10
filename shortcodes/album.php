978841872bae386341124d15b717b2b838402c35
Zack Kollar <rallokkcaz@gmail.com>
2018-12-10 02:44:08 -0800

    Oops removed the wrong line

--- shortcodes/album.php
+++ shortcodes/album.php
@@ -63,6 +63,8 @@ function discogs_album_shortcode($_attributes) {
 
 function discogs_album_shortcode_stylesheets() {
     $plugin_url = plugin_dir_url(__FILE__);
+
     wp_register_style('discogs-album-shortcode-style', $plugin_url . '../stylesheets/discogs-album.css');
+    wp_enqueue_style('discogs-album-shortcode-style');
 }
 ?>
