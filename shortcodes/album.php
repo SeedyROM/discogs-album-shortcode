eaf46791a7489b2214ccdc63aec7673acee26410
Zack Kollar <rallokkcaz@gmail.com>
2018-12-09 21:04:47 -0800

    Working on rendering html

--- shortcodes/album.php
+++ shortcodes/album.php
@@ -9,18 +9,25 @@ function shortcode_error($error) {
 function query_discogs_api($id) {
     $uri = 'https://api.discogs.com/releases/' . $id;
 
-    $request = wp_remote_get($id);
+    $request = wp_remote_get($uri);
     if(is_wp_error($request)) {
         return null;
     }
 
     $response = wp_remote_retrieve_body($request);
-    $album_data = json_decode($response);
+    $release_data = json_decode($response);
 
-    return $album_data;
+    return $release_data;
 }
 
-// The actual shortcode
+function render_html($data) {
+    return <<<HTML
+        <div class="discogs-album">
+            {$data->artists[0]->name}
+        </div>
+HTML;
+}
+
 function discogs_album_shortcode($_attributes) {
     // Set defaults
     $attributes = shortcode_atts(array(
@@ -28,17 +35,16 @@ function discogs_album_shortcode($_attributes) {
         'size' => 'large'
     ), $_attributes);
 
-    // Make sure an id is given.
     if($attributes['id'] == null) {
         return shortcode_error('ID required');
     }
 
-    $album_data = query_discogs_api($attributes['id']);
+    $album = query_discogs_api($attributes['id']);
 
-    if($album_data == null) {
+    if($album == null) {
         return shortcode_error('Invalid discogs ID');
     }
 
-    return $album_data['description'];
+    return render_html($album);
 }
 ?>
