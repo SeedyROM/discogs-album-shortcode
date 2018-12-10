7f34fe52928e044134565c283a510aca01c7d7f5
Zack Kollar <rallokkcaz@gmail.com>
2018-12-09 20:53:23 -0800

    Updated query_discogs_api to use wp_remote_get instead of curl

--- shortcodes/album.php
+++ shortcodes/album.php
@@ -7,35 +7,20 @@ function shortcode_error($error) {
 }
 
 function query_discogs_api($id) {
-    // Build the uri for the api call
     $uri = 'https://api.discogs.com/releases/' . $id;
 
-    // Setup the api client
-    $client = curl_init($uri);
-    curl_setopt_array($client, array (
-        CURLOPT_USERAGENT => 'Discogs Album Shortcode',
-        CURLOPT_FAILONERROR => true,
-    ));
-
-    // Get our response data or handle errors.
-    $response = curl_exec($client);
-    if (curl_error($client)) {
-        $error = curl_error($client);
-    }
-    curl_close($client);
-    
-    // If the request failed, return null
-    if (isset($error)) {
+    $request = wp_remote_get($id);
+    if(is_wp_error($request)) {
         return null;
     }
 
-    // Parse out important information
-    // TODO
-    $album_data = array();
+    $response = wp_remote_retrieve_body($request);
+    $album_data = json_decode($response);
 
     return $album_data;
 }
 
+// The actual shortcode
 function discogs_album_shortcode($_attributes) {
     // Set defaults
     $attributes = shortcode_atts(array(
@@ -54,6 +39,6 @@ function discogs_album_shortcode($_attributes) {
         return shortcode_error('Invalid discogs ID');
     }
 
-    return 'Discogs boys!';
+    return $album_data['description'];
 }
 ?>
